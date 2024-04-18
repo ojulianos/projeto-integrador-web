505567dc8dd794a221b92a3760fcdf0037cd5c00
Juliano Silva <juliano.sil6@gmail.com>
2024-04-18 19:20:55 -0300

    Ajuste caminho css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?d={{ date('dmyHis') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}?d={{ date('dmyHis') }}"> --}}
+    <link rel="stylesheet" href="./css/app.css">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
