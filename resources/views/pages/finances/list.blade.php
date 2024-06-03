dd1a40a027fa59297b96a900fa4bf62fd659f070
Paulo Cesar Dal Ponte <paulocesardalponte@gmail.com>
2024-06-02 22:07:37 -0300

    front

--- resources/views/pages/finances/list.blade.php
+++ resources/views/pages/finances/list.blade.php
@@ -127,8 +127,8 @@
                     <button 
                         type="button"
                         onclick="payFinance({{ $finance->id }})"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-green-600 rounded-lg hover:bg-green-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-green-600 rounded-lg hover:bg-green-600 focus:ring-4 focus:bg-green-800 dark:focus:bg-green-800 hover:bg-green-800 ">
+                        <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path fill="#62ff00" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"></path>               
                         </svg>
                         Pagar
@@ -137,7 +137,7 @@
                         type="button"
                         onclick="formFinance({{ $finance->id }})"
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"clip-rule="evenodd"></path>
                         </svg>
@@ -147,7 +147,7 @@
                         type="button"
                         onclick="deleteFinance({{ $finance->id }})"
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
-                        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                         </svg>
                         Excluir
