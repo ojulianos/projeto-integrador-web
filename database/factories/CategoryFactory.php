8579e38aed6eb20029b52c2bd5ae4f733765f57e
lucaspaixao-code <lucassernajoto8@gmail.com>
2024-06-05 23:08:09 -0300

    Factory categoria

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => Str::random(10),
+            'description' =>Str::random(25),
+            'age_max' => random_int(10,18), // password
+            'age_min' => random_int(1,9),
+        ];
+    }
+}
