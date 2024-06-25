cd60dabdf082bce94a2f0c504dab65c781d003dc
lucaspaixao-code <lucassernajoto8@gmail.com>
2024-06-25 01:15:10 -0300

    Add relatorio aluno x presença

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -16,4 +16,19 @@ class Student extends Model
             "SELECT name, surname, '{$posicao}' as posicao  FROM students where position_1 = '{$posicao}' or position_2 = '{$posicao}'"
         );
     }
+
+
+    public function relatorioAlunosPresenca($dataIni, $dataFim)
+    {
+        return DB::select(
+            "SELECT (SELECT concat(name, ' ', surname) FROM students WHERE students.id = presences.student_id) AS StudentName,
+                    COUNT(*) AS PresenceCount,
+                    DATE_FORMAT('{$dataIni}', '%d/%m/%Y') dataIni,
+                    DATE_FORMAT('{$dataFim}', '%d/%m/%Y') dataFim
+               FROM presences
+              WHERE presence_date BETWEEN '{$dataIni}' AND '{$dataFim}'
+              GROUP BY student_id"
+        );
+    }
+
 }
